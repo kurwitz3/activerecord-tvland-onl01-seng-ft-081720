7d122fae88071aedb1af225ccab20094d3ffc77a
Richard Kurwitz <kurwitz3@aol.com>
2020-09-25 00:58:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,7 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network 
   def actors_list 
-    Character.select.where("actor_id: = show_id")
+    Character.collect.where("actor_id: = show_id") do |m|
 binding.pry       
   
  
