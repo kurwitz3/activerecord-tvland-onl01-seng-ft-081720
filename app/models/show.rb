24e987408f72b47703d4baa2c983b1479a7196e4
Richard Kurwitz <kurwitz3@aol.com>
2020-09-25 00:45:38 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  has_many  :characters 
+  has_many :actors, through: :characters
+  belongs_to :network 
+  def actors_list 
+    a = []
+    Show.all
+binding.pry       
   
+ 
+end
 end
